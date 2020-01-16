d92375326a88baf9e5fd1516ff9a2fb8942e4dca
clarodanesh <danesh.iqbal@clarosoftware.com>
2020-01-16 12:25:56 -0800

    added logo with css

--- application/views/login.php
+++ application/views/login.php
@@ -4,6 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 	<title>Login - AutoDoc</title>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/autodoc-main.css">
     <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
@@ -13,6 +15,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div id="auth-form-container">
     <form class="auth-form" action="">
         <div id="auth-form-title">Login to AutoDoc</div>
+        <img id="auth-form-logo" src="<?php echo base_url(); ?>images/logo.png"/>
         <div id="auth-form-hint">Use your Email and Password</div>
         <div class="auth-form-section">
             <input class="auth-form-input" type="text" name="email" placeholder="Email">
@@ -22,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <input class="auth-form-input" type="password" name="password" placeholder="Password">
         </div>
 
-        <div id="auth-form-question" style="font-size: 0.8rem;font-family: 'Ubuntu', sans-serif;/*! margin-bottom: 30px; */margin-top: 20px;">Not signed up ? <a href="#">Sign up</a></div>
+        <div id="auth-form-question">Not signed up? <a href="#">Sign up</a></div>
 
         <input class="auth-form-primary-btn" type="submit" value="Login">
     </form>
